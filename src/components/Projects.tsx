52c75fcd26977f895b3ed61837842c90cb8debc3
Saheedatt <afolabisaheedat1@gmail.com>
2024-11-02 18:38:14 +0100

    Upated padding

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -50,7 +50,7 @@ const Projects = () => {
   ];
 
   return (
-    <div className="max-w-4xl mx-auto p-6">
+    <div className="max-w-4xl mx-auto px-6">
        <div className="flex justify-center mb-8">
         <Navigation />
       </div>
